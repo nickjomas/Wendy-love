cfc0ecab974ab4e344c2c795664fe507258485de
nickjomas <oyonoeffe11@gmail.com>
2022-10-11 15:47:09 +0200

    ajout du fiché php

---
+++ php/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>voiranime</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <a href="" class="logo"><span>VOIR</span>ANIME</a>
+
+        <ul class="ul1">
+            <li><a href="">ACCEIL</a></li>
+            <li><a href="">LISTE</a></li>
+            <li><a href="">NOUVEAU</a></li>
+            <li><a href="">PROCHAINEMENT</a></li>
+            <li><a href="">RECHERCHE AV</a></li>
+        </ul>
+    </nav>
+
+    <div>
+        <ul class="ul2">
+            <li><a href="">ACCTION</a></li>
+            <li><a href="">AVENTURE</a></li>
+            <li><a href="">CHINESE</a></li>
+            <li><a href="">COMEDY</a></li>
+            <li><a href="">DRAMA</a></li>
+            <li><a href="">ICCHI</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <ul class="ul3">
+            <li><a href="">HOMME</a> /</li>
+            <li><a href="">Isekai Yakkyoku</a> / </li>
+            <li class="li1"> Isekai Yakkyoku - 10 VOSTFR - 10</li>
+        </ul>
+    </div>
+
+
+    <div class="box">
+    <div class="animation">
+        <div class="cercle"> <p>MON ANIMATION</p></div>
+    </div>
+    </div>
+
+    <div class="box1">
+        <div class="animation1">
+            <div class="cercle1"> <p>CHINESE</p></div>
+        </div>
+    </div>
+
+    <div class="coeur"></div>
+   
+</body>
+</html>
